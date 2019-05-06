201e0e2db5e2415805b27756b19a2d3f7fe46213
Gordon <gordonzhou68@outlook.com>
2019-05-06 17:20:24 +0000

    part4 completed

---
+++ forthfunction.js
@@ -0,0 +1,30 @@
+function getHTML(options, callback) {
+
+    var https =require('https');
+
+    var completedata = "";
+
+    https.get(options, function (response) {
+        response.on('data', function (data) {
+            completedata += data;
+            console.log('Chunk Received. Length:', data.length, '\n');
+          });
+    
+        response.on('end', function(data) {
+            callback(completedata)
+          });
+
+    })
+}
+  
+function printHTML (html) {
+    console.log(html);
+  }
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step3.html'
+  };
+
+
+  getHTML(requestOptions, printHTML);
